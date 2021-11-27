12fe22c25895a0d7b853522ac7a72ffce9e5f36e
TinasheMTapera <tinashemtapera@gmail.com>
2021-11-26 21:00:03 -0500

    try new google oauth

--- run.R
+++ run.R
@@ -4,6 +4,17 @@
 # this file renders both the resume and CV in PDF and HTML format
 
 # render one page resume
+
+read_subsheet <- function(subsh){
+  
+  googlesheets4::gs4_deauth()
+  sh <- Sys.getenv("RESUMECV_URL")
+  googlesheets4::read_sheet(sh, sheet = subsh)
+  
+}
+
+read_subsheet("jobs")
+
 rmarkdown::render(input = here::here("one_page_resume", "one_page_resume_pagedown.Rmd"),
                   output_file = "tinashemtapera_cv.html",
                   output_options = list(
