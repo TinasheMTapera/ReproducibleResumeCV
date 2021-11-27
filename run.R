c48c58c327ce7bc0c2a4dcc877a0daefe80ea33d
TinasheMTapera <tinashemtapera@gmail.com>
2021-11-26 22:28:59 -0500

    add long form cv

--- run.R
+++ run.R
@@ -3,10 +3,10 @@
 
 # this file renders both the resume and CV in PDF and HTML format
 
-# render one page resume
+# render one page resume HTML
 here::here("/root/project/")
 rmarkdown::render(input = here::here("one_page_resume", "one_page_resume_pagedown.Rmd"),
-                  output_file = "tinashemtapera_cv.html",
+                  output_file = "tinashemtapera_resume.html",
                   output_options = list(
                     css = c('css/custom_resume.css', 'css/styles_html.css', 'resume'),
                     self_contained = TRUE
@@ -17,9 +17,9 @@ rmarkdown::render(input = here::here("one_page_resume", "one_page_resume_pagedow
 )
 
 
-# render long form cv
+# render one page resume PDF
 rmarkdown::render(input = here::here("one_page_resume", "one_page_resume_pagedown.Rmd"),
-                  output_file = "tinashemtapera_cv.html",
+                  output_file = "tinashemtapera_resume.html",
                   output_options = list(
                     css = c('css/custom_resume.css', 'css/styles_pdf.css', 'resume')
                   ),
@@ -27,3 +27,12 @@ rmarkdown::render(input = here::here("one_page_resume", "one_page_resume_pagedow
                     doctype = "PDF"
                   )
 )
+
+# render long form cv
+rmarkdown::render(input = here::here("long_form_cv", "long_form_cv_vitae.Rmd"),
+                  output_file = "tinashemtapera_cv.html",
+                  params = list(
+                    googlescholarid = "9N6a5jsAAAAJ"
+                  )
+)
+
