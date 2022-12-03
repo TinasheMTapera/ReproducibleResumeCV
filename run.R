d8883eda03d176f77acb48b53674ff18831b8610
TinasheMTapera <tinashemtapera@gmail.com>
2022-12-02 19:55:09 -0500

    try with github actions

--- run.R
+++ run.R
@@ -5,28 +5,28 @@
 
 # render one page resume HTML
 here::here("/root/project/")
-rmarkdown::render(input = here::here("one_page_resume", "one_page_resume_pagedown.Rmd"),
-                  output_file = "tinashemtapera_resume.html",
-                  output_options = list(
-                    css = c('css/custom_resume.css', 'css/styles_html.css', 'resume'),
-                    self_contained = TRUE
-                  ),
-                  params = list(
-                    doctype = "HTML"
-                  )
-)
+# rmarkdown::render(input = here::here("one_page_resume", "one_page_resume_pagedown.Rmd"),
+#                   output_file = "tinashemtapera_resume.html",
+#                   output_options = list(
+#                     css = c('css/custom_resume.css', 'css/styles_html.css', 'resume'),
+#                     self_contained = TRUE
+#                   ),
+#                   params = list(
+#                     doctype = "HTML"
+#                   )
+# )
 
 
 # render one page resume PDF
-rmarkdown::render(input = here::here("one_page_resume", "one_page_resume_pagedown.Rmd"),
-                  output_file = "tinashemtapera_resume.html",
-                  output_options = list(
-                    css = c('css/custom_resume.css', 'css/styles_pdf.css', 'resume')
-                  ),
-                  params = list(
-                    doctype = "PDF"
-                  )
-)
+# rmarkdown::render(input = here::here("one_page_resume", "one_page_resume_pagedown.Rmd"),
+#                   output_file = "tinashemtapera_resume.html",
+#                   output_options = list(
+#                     css = c('css/custom_resume.css', 'css/styles_pdf.css', 'resume')
+#                   ),
+#                   params = list(
+#                     doctype = "PDF"
+#                   )
+# )
 
 # render long form cv
 rmarkdown::render(input = here::here("long_form_cv", "long_form_cv_vitae.Rmd"),
